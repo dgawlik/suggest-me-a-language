e9e760470990aa1b99d55f922fda719d415e48cc
Dominik Gawlik <dominik.gawlik1@gmail.com>
2022-12-26 13:17:27 +0100

    database integrity

---
+++ src/test/kotlin/org/dgawlik/DatabaseTest.kt
@@ -0,0 +1,39 @@
+package org.dgawlik
+
+import org.dgawlik.domain.BinaryField
+import org.dgawlik.domain.Feature
+import org.dgawlik.domain.Language
+import org.dgawlik.parsing.Parser
+import org.junit.jupiter.api.Test
+
+internal class DatabaseTest {
+
+    val parser: Parser
+
+    init {
+        val db = BinaryField::class.java.getResource("/Database.md")!!.readText()
+        parser = Parser(db)
+        parser.parse()
+    }
+
+    fun containsExactly(languages: Array<Language>, feature: Feature, names: Set<String>): Boolean{
+        val found: MutableSet<String> = mutableSetOf()
+
+        languages.forEach {
+            if (it.features.any { feat ->  feat.feature.id == feature.id && feat.value > 0} ){
+                found += it.name
+            }
+        }
+
+        return names == found
+    }
+
+    @Test
+    fun all_valid_languages_with_aspects(){
+        val feature = parser.features.find { it.id == "LANG27" }!!
+
+        val validLanguages = setOf("Java", "C#", "Kotlin", "Scala", "Groovy")
+
+        assert(containsExactly(parser.languages, feature, validLanguages))
+    }
+}
