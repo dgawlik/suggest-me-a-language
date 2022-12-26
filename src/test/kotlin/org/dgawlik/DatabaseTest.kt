b21c1ae27a0ec080c967c8501e5cdba65e010bd0
Dominik Gawlik <dominik.gawlik1@gmail.com>
2022-12-26 14:48:44 +0100

    database integrity

--- src/test/kotlin/org/dgawlik/DatabaseTest.kt
+++ src/test/kotlin/org/dgawlik/DatabaseTest.kt
@@ -28,6 +28,15 @@ internal class DatabaseTest {
         return names == found
     }
 
+    fun noneOf(languages: Array<Language>, feature: Feature, names: Set<String>): Boolean{
+        languages.forEach {
+            if (it.features.any { feat ->  feat.feature.id == feature.id && feat.value > 0}  && names.contains(it.name)){
+                return false
+            }
+        }
+        return true
+    }
+
     @Test
     fun all_valid_languages_with_aspects(){
         val feature = parser.features.find { it.id == "LANG27" }!!
@@ -45,5 +54,14 @@ internal class DatabaseTest {
             "Objective C", "Lua", "Lisp", "Scheme", "Haxe", "TypeScript")
 
         assert(containsExactly(parser.languages, feature, validLanguages))
+    }
+
+    @Test
+    fun none_of_not_crossplatform_languages(){
+        val feature = parser.features.find { it.id == "TARGET2" }!!
+
+        val invalidLanguages = setOf("C#", "F#", "Swift", "Objective C", "Visual Basic", "SAS")
+
+        assert(noneOf(parser.languages, feature, invalidLanguages))
     }
 }
